72f3a4880537f99dc1ff19fbd1de6dec881ca328
Anthony <anthony@MacBook-Air-Anthony.local>
2022-07-09 21:05:56 +0300

    add logic

--- app.js
+++ app.js
@@ -1,39 +1,73 @@
-console.log("Hello");
 const boxes = document.querySelectorAll(".grid-box");
-const boxesArr = [];
-boxes.forEach(el => boxesArr.push(el));
-console.log(boxes);
+const checkButton = document.querySelector(".key-check");
+const grid = document.querySelector(".grid");
+const gameboard = [];
+
+for(let i = 6; i > 0; --i){
+    const row = [];
+    gameboard.push(row);
+    for(let i = 5; i > 0; --i){
+        const gridBox = document.createElement("div");
+        gridBox.classList.add("grid-box");
+        row.push(gridBox);
+    };
+};
+
+for(let row of gameboard){
+    for(let box of row){
+        grid.appendChild(box);
+    }
+};
 
 const generatedWord = "парта";
+const hashMap = {};
+    const arr = generatedWord.split("");
+    for(let i in arr){
+        hashMap[arr[i]] = true;
+    };
 
 let userWord = "";
-let boxPosition = 0;
+let attempt = 0;
+let position = 0;
 document.querySelector(".keyboard").addEventListener("click", (e)=>{
-    if(e.target.classList.contains("key")){
-        if(userWord.length !== 5){
-            boxPosition += 1; 
-            userWord += e.target.innerText;
-            console.log(boxPosition % 5);
-            boxes[boxPosition-1].innerText = e.target.innerText;
-        }
+    if(e.target.classList.contains("key") && userWord.length !== 5){
+        userWord += e.target.innerText;
+        gameboard[attempt][position].innerText = e.target.innerText;
+        position += 1;
     };
 });
 
-document.querySelector(".key-check").addEventListener("click", ()=>{
-    const hashMap = {};
-    const arr = generatedWord.split("");
-    for(let i in arr){
-        hashMap[arr[i]] = i;
-    };
-
-    if(userWord.length === 5){
-        for(let i = 0; i < userWord.length; ++i){
-            if(userWord[i].toLowerCase() === generatedWord[i].toLowerCase()){
-                boxes[i].classList.add("green");
-            } else if(hashMap[userWord.toLowerCase()[i]]){
-                boxes[i].classList.add("yellow");
+checkButton.addEventListener("click", ()=>{
+    if(userWord.length === 5) {
+        if(userWord.toLowerCase() === generatedWord){
+            console.log("Слово угадано", attempt+1);
+            for(let box of gameboard[attempt]){
+                box.classList.add("green");
             }
+            checkButton.setAttribute("disabled", true);
+        } else {
+            for(let i = 0; i < userWord.length; ++i){
+                if(userWord.toLowerCase()[i] === generatedWord[i]){
+                    gameboard[attempt][i].classList.add("green");
+                } else if(hashMap[userWord.toLowerCase()[i]]){
+                    gameboard[attempt][i].classList.add("yellow");
+                } else {
+                    gameboard[attempt][i].classList.add("gray");
+                }
+            }
+            attempt += 1;
+            userWord = "";
+            position = 0;
         }
     }
 });
+
+document.querySelector(".key-large").addEventListener("click", (e) => {
+    userWord = userWord.split("").splice(0, userWord.length - 1).join("");
+    gameboard[attempt][position-1].innerText = "";
+    if(position > 1) {
+        position -= 1;
+    }
+    console.log(userWord);
+})
 
