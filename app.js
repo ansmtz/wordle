a48473ede00d8ca234234f753308c3f97b2aa068
Anthony <anthony@MacBook-Air-Anthony.local>
2022-07-23 00:38:55 +0300

    words

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const boxes = document.querySelectorAll(".grid-box");
 const checkButton = document.querySelector(".key-check");
 const grid = document.querySelector(".grid");
 const gameboard = [];
-const words = ["рамка", "лавка", "мусор"];
 
 for(let i = 6; i > 0; --i){
     const row = [];
@@ -23,16 +22,17 @@ for(let row of gameboard){
 const generatedWord = words[Math.floor(Math.random()*words.length)];
 console.log(generatedWord);
 const hashMap = {};
-    const arr = generatedWord.split("");
-    for(let i in arr){
-        hashMap[arr[i]] = true;
-    };
+const arr = generatedWord.split("");
+for(let i in arr){
+    hashMap[arr[i]] = true;
+};
 
 let userWord = "";
 let attempt = 0;
 let position = 0;
+let isGameOver = false;
 document.querySelector(".keyboard").addEventListener("click", (e)=>{
-    if(e.target.classList.contains("key") && userWord.length !== 5){
+    if(e.target.classList.contains("key") && userWord.length !== 5 && attempt <= 5 && !isGameOver){
         userWord += e.target.innerText;
         gameboard[attempt][position].innerText = e.target.innerText;
         position += 1;
@@ -40,29 +40,36 @@ document.querySelector(".keyboard").addEventListener("click", (e)=>{
 });
 
 checkButton.addEventListener("click", ()=>{
-    if(userWord.length === 5) {
-        if(userWord.toLowerCase() === generatedWord){
-            console.log("Слово угадано", attempt+1);
-            for(let box of gameboard[attempt]){
-                box.classList.add("green");
-            }
-            checkButton.setAttribute("disabled", true);
+    if(userWord.length === 5 && attempt <= 5) {
+        checkWord(userWord.toLowerCase(), generatedWord);    
+        attempt += 1;
+        userWord = "";
+        position = 0;
+    } 
+});
+
+function checkWord(userWord, generatedWord){
+    markLetters(userWord, generatedWord);
+    if(userWord === generatedWord && attempt <= 5){
+        console.log("Слово угадано", attempt+1);
+        isGameOver = true;
+    } else if(userWord !== generatedWord && attempt === 5){
+        console.log("Игра окончена");
+        isGameOver = true;
+    }
+};
+
+function markLetters(userWord, generatedWord){
+    for(let i = 0; i < userWord.length; ++i){
+        if(userWord[i] === generatedWord[i]){
+            gameboard[attempt][i].classList.add("green");
+        } else if(hashMap[userWord[i]]){
+            gameboard[attempt][i].classList.add("yellow");
         } else {
-            for(let i = 0; i < userWord.length; ++i){
-                if(userWord.toLowerCase()[i] === generatedWord[i]){
-                    gameboard[attempt][i].classList.add("green");
-                } else if(hashMap[userWord.toLowerCase()[i]]){
-                    gameboard[attempt][i].classList.add("yellow");
-                } else {
-                    gameboard[attempt][i].classList.add("gray");
-                }
-            }
-            attempt += 1;
-            userWord = "";
-            position = 0;
+            gameboard[attempt][i].classList.add("gray");
         }
     }
-});
+}
 
 document.querySelector(".key-large").addEventListener("click", (e) => {
     userWord = userWord.split("").splice(0, userWord.length - 1).join("");
